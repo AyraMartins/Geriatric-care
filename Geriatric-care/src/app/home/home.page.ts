cfdbab6a4dbca9b00215c4aff580dede3bd5888f
AyraMartins <168045198+AyraMartins@users.noreply.github.com>
2026-04-24 20:04:26 -0300

    24-04-26
    Adição de itens a página inicial e teste do esp32

--- Geriatric-care/src/app/home/home.page.ts
+++ Geriatric-care/src/app/home/home.page.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import {
-  IonicModule
-} from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +8,29 @@ import {
   standalone: true,
   imports: [IonicModule]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  bpm: number = 0;
+
   constructor() {}
+
+buscarBpm() {
+  fetch('http://192.168.0.51/bpm', { mode: 'no-cors' })
+    .then(res => res.text())
+    .then(valor => {
+      const numero = parseInt(valor);
+      if (!isNaN(numero)) {
+        this.bpm = numero;
+      }
+    })
+    .catch(err => {
+      console.error('Erro:', err);
+    });
 }
 
+  ngOnInit() {
+    setInterval(() => {
+      this.buscarBpm();
+    }, 2000); // a cada 2 segundos
+  }
+}
