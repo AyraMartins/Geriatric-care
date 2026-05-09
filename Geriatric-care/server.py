f3a43c5c1c8c7e24086f6526852b07e5d84d6425
AyraMartins <168045198+AyraMartins@users.noreply.github.com>
2026-05-09 12:23:39 -0300

    09-05-06
    tentando conectar a uma internet sem senha

--- Geriatric-care/server.py
+++ Geriatric-care/server.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import mysql.connector
+from datetime import datetime
 
-# --------------------
-# APP PRIMEIRO
-# --------------------
+# -------------------
+# APP
+# -------------------
+
 app = Flask(__name__)
 CORS(app)
 
-# --------------------
-# BANCO XAMPP (MySQL)
-# --------------------
+# -------------------
+# MYSQL
+# -------------------
+
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -18,59 +21,114 @@ db = mysql.connector.connect(
     database="banco_geriatric_care"
 )
 
-# --------------------
-# BPM (ESP32)
-# --------------------
+# -------------------
+# DADOS BPM
+# -------------------
+
 dados = []
+
+# -------------------
+# RECEBER BPM
+# -------------------
 
 @app.route('/bpm')
 def bpm():
-    valor = request.args.get('valor')
-    if valor:
-        dados.append(int(valor))
-        print("Recebido BPM:", valor)
 
-    return {"ok": True}
+    valor = request.args.get('valor')
+
+    if valor:
+
+        bpm = int(valor)
+
+        dados.append({
+            "valor": bpm,
+            "hora": datetime.now().strftime("%H:%M:%S")
+        })
+
+        print("BPM:", bpm)
+
+    return jsonify({
+        "ok": True
+    })
+
+# -------------------
+# PEGAR BPM
+# -------------------
 
 @app.route('/')
 def home():
+
+    ultimo = dados[-1] if dados else {
+        "valor": 0,
+        "hora": "--:--:--"
+    }
+
     return jsonify({
-        "bpm": dados[-1] if dados else 0,
+        "ultimo_bpm": ultimo,
         "historico": dados[-10:]
     })
 
-# --------------------
-# CADASTRO CUIDADOR
-# --------------------
+# -------------------
+# CADASTRAR CUIDADOR
+# -------------------
+
 @app.route('/cuidador', methods=['POST'])
 def criar_cuidador():
+
     try:
+
         data = request.json
 
         cursor = db.cursor()
 
         sql = """
-            INSERT INTO cuidador (nm_cuidador, email_cuidador, tel_cuidador, cd_tipo)
-            VALUES (%s, %s, %s, %s)
+            INSERT INTO cuidador
+            (
+                nm_cuidador,
+                email_cuidador,
+                tel_cuidador,
+                cd_tipo
+            )
+            VALUES
+            (
+                %s,
+                %s,
+                %s,
+                %s
+            )
         """
 
         cursor.execute(sql, (
+
             data['nome'],
             data['email'],
             data['telefone'],
             1
+
         ))
 
         db.commit()
 
-        return jsonify({"msg": "Cuidador criado com sucesso"}), 200
+        return jsonify({
+            "msg": "Cuidador criado"
+        }), 200
 
     except Exception as e:
-        print("ERRO:", e)
-        return jsonify({"erro": str(e)}), 500
 
-# --------------------
-# START SERVER
-# --------------------
+        print(e)
+
+        return jsonify({
+            "erro": str(e)
+        }), 500
+
+# -------------------
+# START
+# -------------------
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
